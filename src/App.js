d8fd381a3d4b7e0ad972c50cc2ceb0d582198968
wicho1001 <wichoguerrero747@gmail.com>
2019-10-22 23:20:28 -0500

    feat: Agregando apollo client, y agregando primer query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const getPokemons = gql`
+  query{
+    pokemon(name: "Pikachu") {
+      id
+      number
+      name
+    }
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Query query={getPokemons}>
+      {({loading, error, data}) => {
+        console.log(data)
+        if (loading) return <div>loading...</div>
+        if (error) return <div>error</div>
+        return(
+          <div>{data.pokemon.name}</div>
+        )
+      }}
+    </Query>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({uri:"https://graphql-pokemon.now.sh"})
+
+const ApolloApp = App => (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+)
+
+render(ApolloApp(App), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
